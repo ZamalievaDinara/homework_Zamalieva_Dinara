04446ed0a2fc4eca76b783bf9cfd2bcb0c9e15d0
ZamalievaDinara <91804152+ZamalievaDinara@users.noreply.github.com>
2021-10-03 00:19:28 +0300

    21.09.21

---
+++ dz1.cpp
@@ -0,0 +1,16 @@
+﻿// 1
+#include <iostream>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "Rus");
+	int a;
+	cout << "Введите число: ";
+	cin >> a; 
+	if (a > 99 && a < 1000)
+		cout << "Введено трехзначное число";
+	else
+		cout << "Введено не трехзначное число";
+
+	return 0;
+}
+
